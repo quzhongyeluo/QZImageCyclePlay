6cac851581707b72c68a23dab19439ed37d6da9b
曲终叶落 <quzhongyeluo@quzhongyeluo.lan>
2017-09-17 16:54:56 +0800

    初始化项目

---
+++ QZImageCyclePlay/QZImageCyclePlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QZImageCyclePlay
+//
+//  Created by 曲终叶落 on 2017/9/17.
+//  Copyright © 2017年 曲终叶落. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QZImageCyclePlay/QZImageCyclePlay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QZImageCyclePlay
+//
+//  Created by 曲终叶落 on 2017/9/17.
+//  Copyright © 2017年 曲终叶落. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
